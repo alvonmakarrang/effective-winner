c7418af7b82283075b28410ebd6a5173c232d9c4
alvonmakarrang <128981449+alvonmakarrang@users.noreply.github.com>
2023-03-26 20:53:22 +0700

    style.css
    file style.css

---
+++ style.css
@@ -0,0 +1,38 @@
+!DOCTYPE html>
+<html>
+<head>
+	<title>SEARCH MUSIC</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1>CARI MUSIK KAMU DISINI!</h1>
+	<form method="GET" action="">
+		<input type="text" name="search" id="search">
+		<input type="submit" value="Cari">
+	</form>
+	<?php
+		if(isset($_GET['search'])) {
+			$searchTerm = urlencode($_GET['search']);
+			$url = "https://itunes.apple.com/search?term=$searchTerm&media=music";
+			$response = file_get_contents($url);
+			$result = json_decode($response);
+
+			if(!empty($result->results)) {
+				echo "<ul>";
+				foreach($result->results as $item) {
+					echo "<li>";
+					echo "<a href='" . $item->trackViewUrl . "' target='_blank'>";
+					echo "<img src='" . $item->artworkUrl100 . "' alt='" . $item->trackName . "'>";
+					echo "</a>";
+					echo "<h2>" . $item->trackName . "</h2>";
+					echo "<p>" . $item->artistName . "</p>";
+					echo "</li>";
+				}
+				echo "</ul>";
+			} else {
+				echo "Tidak ada hasil ditemukan.";
+			}
+		}
+	?>
+</body>
+</html>
